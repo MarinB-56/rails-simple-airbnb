773e2799af8345e22e71f64e6723c97ab7406188
Marin Boone <150065021+MarinB-56@users.noreply.github.com>
2024-09-26 19:21:37 +0200

    Create seed for 10 flats

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+puts "Cleaning database..."
+Flat.destroy_all
+
+puts "Creating flats..."
+10.times do
+  flat = Flat.new(
+    name: "#{Faker::Movies::HarryPotter.character}'s house",
+    address: "#{Faker::Movies::HarryPotter.house}, #{Faker::Movies::HarryPotter.location}",
+    description: Faker::Movies::HarryPotter.quote,
+    price_per_night: Faker::Number.between(from: 50, to: 300),
+    number_of_guests: Faker::Number.between(from: 1, to: 12)
+  )
+  puts "Created #{flat.name}"
+  flat.save
+end
+
+# Faker::Lorem.paragraphs
